d885f0a9d2ac0b45609c8a760cb4e1a4887568fb
Eric Lee <e811226@gmail.com>
2015-03-14 13:53:27 -0700

    sign up ui wire frame

--- Sample/Sample/BumpViewController.swift
+++ Sample/Sample/BumpViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class BumpViewController: UIViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,7 +20,6 @@ class BumpViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     
 
     /*
